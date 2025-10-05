38ef2d756cada1ed2788e746c9b9f893df1475cc
Hima <hima246868@gmail.com>
2025-10-05 21:36:19 +0300

    add package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-}
+}gradlew signingReport
+
 
 android {
     namespace = "com.example.chatat"
@@ -24,7 +28,7 @@ android {
         applicationId = "com.example.chatat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk =23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'AsrooStoreApp.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AsrooStoreApp());
 }
 
